8a5ca991e32280209a0841a89a6c29bf0a1d3c0e
vaspurak <vaspurakavdalyani@gmail.com>
2025-02-19 14:36:38 +0100

    furst commmit

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import svgr from "vite-plugin-svgr";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), svgr()],
+  server: {
+    open: true,
+    port: 8000,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
