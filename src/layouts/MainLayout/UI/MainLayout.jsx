bf457018a5090a39139c5fffd269e76594d5d15d
vaspurak <vaspurakavdalyani@gmail.com>
2025-02-21 13:49:28 +0100

    add MainLayout component and global styles; remove unused App component and styles

---
+++ src/layouts/MainLayout/UI/MainLayout.jsx
@@ -0,0 +1,20 @@
+import { Outlet } from "react-router-dom";
+import cls from "./MainLayout.module.scss";
+const MainLayout = () => {
+  return (
+    <div id="app" className="app">
+      <p>Header</p>
+      <main className={cls.main}>
+        <div className={cls.container}>
+          <div className={cls.body}>
+            <p>Navbar</p>
+            <Outlet />
+          </div>
+        </div>
+      </main>
+      <p>Footer</p>
+    </div>
+  );
+};
+
+export { MainLayout };
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom"; // Импортируй из 'react-router-dom'
+import { MainLayout } from "./layouts/MainLayout"; // Используй именованный импорт или экспорт по умолчанию в зависимости от твоего файла
+import "./assets/styles/index.scss";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: <div>HOME</div>,
+      },
+      {
+        path: "/pizzas",
+        element: <div>PIZZAS</div>,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
